1fe50d78dbab1f651b5fdea7cfc6bdd5a13e5cda
AngeloC <82951135+AngeloC3@users.noreply.github.com>
2022-04-25 23:22:32 -0400

    Delete startup.ps1

--- startup.ps1
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-$Env:mongodb_URI = "mongodb+srv://Angelo:angeloadmin@cluster0.7rexm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-echo "connecting to $mongodb_URI"
-node app.js
